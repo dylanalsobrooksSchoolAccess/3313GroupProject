2348300644ad4c835bf2c74a8308c507c1a52443
dylanalsobrooksSchoolAccess <alsobrooksd1@gator.uhd.edu>
2026-04-13 22:09:42 -0500

    Age Verification Logic

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 "use strict";
 
+var ageModal = document.getElementById("age-verification-modal");
+var yesButton = document.getElementById("btn-yes");
+var noButton = document.getElementById("btn-no");
+
+yesButton.addEventListener('click', function() 
+    {
+        ageModal.style.display = 'none';
+    }
+);
+
+noButton.addEventListener('click', function()
+    {
+        window.location.href = "about:blank"; 
+    }
+);
